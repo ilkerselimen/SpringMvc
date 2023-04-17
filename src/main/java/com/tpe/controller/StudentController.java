6aa6469ae1b371e78923fcb021b00ef7881503da
İlker Şelimen <ilkerselimen@gmail.com>
2023-04-17 23:41:27 +0300

    controller

--- src/main/java/com/tpe/controller/StudentController.java
+++ src/main/java/com/tpe/controller/StudentController.java
@@ -1,7 +1,10 @@
 package com.tpe.controller;
 
+import com.tpe.domain.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -17,13 +20,32 @@ public class StudentController {
     @GetMapping("/hi")
     public ModelAndView sayHi(){
         ModelAndView mav=new ModelAndView();
-        mav.addObject("message","Hi; ");
+        mav.addObject("message","Hi, ");
         mav.addObject("messagebody","I am a Student Management System");
         mav.setViewName("hi");//hi.jsp
         return mav;
     }
     //view resolver mav içindeki view name e göre hi.jsp dosyasını bulur.
     //mav içindeki model ı hi.jsp içerisine bind eder.
+
+    //1-Student Creation
+    //kullanicidan bilgileri almak icin  form gosterelim
+    @GetMapping("/new")//http://localhost:8080/SpringMVC/students/new
+    public String sendStudentForm(@ModelAttribute("student") Student student){
+        return "studentForm";
+    }
+
+    //@ModelAttribute ann:Student formundaki bilgilerle Student tipinde bir obje olusturur,
+    // daha sonra bu objenin kullanilmasini saglar. View ile controller arasinda data transferini saglar.
+
+    //formun submit://http://localhost:8080/SpringMVC/students/saveStudent, method:POST
+    //tum listeyi gosterelim
+    @PostMapping("/saveStudent")
+    public String createStudent(@ModelAttribute Student student){ // @ModelAttribute("student") burada optional
+
+
+        return null;
+    }
 
 
 }
